8f870c8b751a007f132a7ee7efe3908608a53bee
Anuj Vadecha <anujvadecha@Anujs-MacBook-Pro.local>
2023-11-17 20:32:57 -0500

    changes

--- caab-app/src/js/app.js
+++ caab-app/src/js/app.js
@@ -1,26 +1,28 @@
 App = {
   web3: null,
   contracts: {},
-  address: "0x3af7286d20114e40993bb149abadC3DAF40DD263",
+  cstaddress: "0x03Bd3DC08CfB222581D41bd5f41d13ECfb1f24ad",
+  cctaddress: "", 
+  cabaddress: "",
   names: new Array(),
   url: "http://127.0.0.1:7545",
   chairPerson: null,
   currentAccount: null,
 
   init: function () {
-    $.getJSON("../proposals.json", function (data) {
-      var proposalsRow = $("#proposalsRow");
-      var proposalTemplate = $("#proposalTemplate");
-
-      for (i = 0; i < data.length; i++) {
-        proposalTemplate.find(".card-title").text(data[i].name);
-        proposalTemplate.find("img").attr("src", data[i].picture);
-        proposalTemplate.find(".btn-vote").attr("data-id", data[i].id);
-
-        proposalsRow.append(proposalTemplate.html());
-        App.names.push(data[i].name);
-      }
-    });
+    // $.getJSON("../proposals.json", function (data) {
+    //   var proposalsRow = $("#proposalsRow");
+    //   var proposalTemplate = $("#proposalTemplate");
+    //
+    //   for (i = 0; i < data.length; i++) {
+    //     proposalTemplate.find(".card-title").text(data[i].name);
+    //     proposalTemplate.find("img").attr("src", data[i].picture);
+    //     proposalTemplate.find(".btn-vote").attr("data-id", data[i].id);
+    //
+    //     proposalsRow.append(proposalTemplate.html());
+    //     App.names.push(data[i].name);
+    //   }
+    // });
     return App.initWeb3();
   },
 
@@ -30,6 +32,7 @@ App = {
     } else {
       App.web3 = new Web3(App.url);
     }
+    // App.web3 = new Web3(Web3.givenProvider || "ws://localhost:8545");
     ethereum.request({ method: "eth_requestAccounts" });
 
     App.populateAddress();
@@ -37,14 +40,37 @@ App = {
   },
 
   initContract: function () {
-    App.contracts.Ballot = new App.web3.eth.Contract(App.abi, App.address, {});
-    return App.bindEvents();
+    let CSTabidata = null;
+    let CSTabi = $.getJSON("../abis/CST_ERC20.json", function (data) {
+      App.contracts.CAB = new App.web3.eth.Contract(data, App.cabaddress, {});
+      return App.bindEvents();
+
+    });
+    // console.log(CSTabidata);
+    // const CCTabi = $.getJSON("../abis/CCT_ERC721.json", function (data) {return data;});
+    // const CABabi = $.getJSON("../abis/CAB_ERC1155.json", function (data) {return data;});
+    // const CSTabi = require('../abis/CST_ERC20.json');
+    // const CCTabi = require('../abis/CCT_ERC721.json');
+    // const CABabi = require('../abis/CAB_ERC1155.json');
+
+    // console.log(CSTabidata);
+    // App.contracts.Ballot = new App.web3.eth.Contract(App.abi, App.address, {});
+    // App.contracts.CCT = new App.web3.eth.Contract(App.cctabi, App.cctaddress, {});
+    // App.contracts.CAB = new App.web3.eth.Contract(App.cababi, App.cabaddress, {});
+    // return App.bindEvents();
   },
 
   bindEvents: function () {
     $(document).on("click", ".btn-vote", App.handleVote);
     $(document).on("click", "#win-count", App.handleWinner);
     $(document).on("click", "#register", App.handleRegister);
+    $(document).on("click", "#getCSTBalance", App.handleCSTBalance);
+    $(document).on("click", "#getCCTBalance", App.handleCCTBalance);
+    $(document).on("click", "#getCABBalance", App.handleCABBalance);
+    $(document).on("click", "#buyCSTSubmit", App.handleBuyCST);
+    $(document).on("click", "#buyCCTSubmit", App.handleBuyCCT);
+    $(document).on("click", "#rent", App.handleRent);
+    $(document).on("click", "#refreshCloudBalances", App.handleRefreshCloudBalances);
   },
 
   populateAddress: async function () {
@@ -52,21 +78,6 @@ App = {
     App.handler = userAccounts[0];
     document.getElementById("currentUserAddress").innerText =
           "Current User Address: " + App.handler;
-
-    // new Web3(new Web3.providers.HttpProvider(App.url)).eth.getAccounts(
-    //   (err, accounts) => {
-    //     document.getElementById("currentUserAddress").innerText =
-    //       "Current User Address: " + App.handler;
-    //     jQuery("#enter_address").empty();
-    //     for (let i = 0; i < accounts.length; i++) {
-    //       if (App.handler != accounts[i]) {
-    //         var optionElement =
-    //           '<option value="' + accounts[i] + '">' + accounts[i] + "</option";
-    //         jQuery("#enter_address").append(optionElement);
-    //       }
-    //     }
-    //   }
-    // );
   },
 
   handleRegister: function () {
@@ -81,7 +92,46 @@ App = {
         toastr.error(App.getErrorMessage(err), "Reverted!");
       });
   },
-
+  handleCSTBalance: function () {
+    var option = { from: App.handler };
+    App.contracts.CAB.methods.balanceOf(App.handler).call((error, balance) => {
+          if (!error) {
+            document.getElementById("currentCSTBalance").innerText =
+                balance;
+            console.log(`Balance of ${App.handler}: ${balance}`);
+          } else {
+            console.error(error);
+          }}
+        );
+      // .on("receipt", (receipt) => {
+      //   toastr.success("Success! Address: " + App.handler + " has been registered.");
+      // })
+      // .on("error", (err) => {
+      //   toastr.error(App.getErrorMessage(err), "Reverted!");
+      // });
+  },
+  handleCCTBalance: function () {
+    App.contracts.CCT.methods.balanceOf(App.handler).call((error, balance) => {
+      if (!error) {
+        document.getElementById("currentCCTBalance").innerText =
+            balance;
+        console.log(`Balance of ${App.handler}: ${balance}`);
+      } else {
+        console.error(error);
+      }}
+    );
+  },
+  handleCABBalance: function () {
+    App.contracts.CAB.methods.balanceOf(App.handler).call((error, balance) => {
+      if (!error) {
+        document.getElementById("currentCABBalance").innerText =
+            balance;
+        console.log(`Balance of ${App.handler}: ${balance}`);
+      } else {
+        console.error(error);
+      }}
+    );
+  },
   handleVote: function (event) {
     event.preventDefault();
     var proposalId = parseInt($(event.target).data("id"));
@@ -148,52 +198,40 @@ App = {
     }
   },
 
-  abi: [
-    {
-      "inputs": [
-        {
-          "internalType": "uint256",
-          "name": "numProposals",
-          "type": "uint256"
-        }
-      ],
-      "stateMutability": "nonpayable",
-      "type": "constructor"
-    },
-    {
-      "inputs": [],
-      "name": "register",
-      "outputs": [],
-      "stateMutability": "nonpayable",
-      "type": "function"
-    },
-    {
-      "inputs": [],
-      "name": "reqWinner",
-      "outputs": [
-        {
-          "internalType": "uint256",
-          "name": "winningProposal",
-          "type": "uint256"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "uint256",
-          "name": "toProposal",
-          "type": "uint256"
-        }
-      ],
-      "name": "vote",
-      "outputs": [],
-      "stateMutability": "nonpayable",
-      "type": "function"
-    }
-  ],
+  handleBuyCST() {
+    console.log("buy CST")
+  },
+  handleBuyCCT() {
+    console.log("buy CCT")
+    var form = document.getElementById('buyCCTForm');
+
+    // Get form elements
+    var gpu = form.elements['gpu'].value;
+    var processor = form.elements['processor'].value;
+    var ram = form.elements['ram'].value;
+    var cores = form.elements['cores'].value;
+    var os = form.elements['os'].value;
+    var image = form.elements['image'].value;
+
+    // Create an object with the form data
+    var formData = {
+      gpu: gpu,
+      processor: processor,
+      ram: ram,
+      cores: cores,
+      os: os,
+      image: image
+    };
+
+    // Log the form data to the console (you can do something else with it)
+    console.log(formData);
+  },
+  handleRent() {
+    console.log("rent")
+  },
+  handleRefreshCloudBalances() {
+    console.log("refresh cloud balances")
+  }
 };
 
 $(function () {
